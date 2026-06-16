4a3eaff12f88accc7006e6ec53b72779f29c111f
ashaanpy-web <ashaan.py@gmail.com>
2026-06-16 00:39:54 -0700

    added blurred divs

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./index.css";
 export default function App() {
   return (
     <>
-      <div className="bg-slate-200/80 min-h-screen">
-        <div className="absolute z-0">
+      <div className="bg-slate-200/80 min-h-screen relative">
+        <div className="w-full h-full z-0">
           <div className="absolute top-[-20%] left-[-20%] w-150 h-150 rounded-full bg-linear-to-br from-lime-400/40 to-emerald-400/20 blur-3xl pointer-events-none z-0"></div>
           <div className="absolute bottom-[-20%] right-[-10%] w-175 h-175 rounded-full bg-linear-to-tr from-sky-400/40 to-indigo-400/20 blur-3xl pointer-events-none z-0 "></div>
-          <div className="bg-linear-to-br from-orange-400/30 to-red-500/10 h-150 w-150  rounded-full absolute bottom-[10%] right-[10%] z-0"></div>
+          <div className="bg-linear-to-br from-orange-500/60 to-red-600/60 h-150 w-150  rounded-full absolute bottom-[-5%] right-[-5%] z-0 blur-3xl pointer-events-none"></div>
         </div>
         <div className="z-10 bg-white/5 border relative border-white/40 backdrop-blur-3xl w-60 min-h-screen rounded-3xl pr-2 pl-2">
           <div className=" ">
